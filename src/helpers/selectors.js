9e7c8636c9e9a017bb2da22ee7cc9702f0f9eb53
JCPenne <jackcpalmer@icloud.com>
2022-09-15 12:59:22 -0700

    use single State object

---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+export function getAppointmentsForDay(state, day) {
+  const result = [];
+
+  if (state.days.length === 0) return result;
+
+  const filteredDay = state.days.filter(element => element.name === day);
+
+  if (filteredDay.length === 0) return result;
+  const appointmentIDs = filteredDay[0].appointments;
+
+  for (let element in state.appointments) {
+    const appointment = state.appointments[element];
+    appointmentIDs.includes(appointment.id) && result.push(appointment);
+  }
+
+  return result;
+}
