22b8d0e9b15923d4c7173bbee69887993ee58c39
JCPenne <jackcpalmer@icloud.com>
2022-09-30 21:03:22 -0700

    further the application test suite. Also refactor adjustSpots function to no long mutate state

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -10,22 +10,41 @@ export default function useApplicationData() {
   });
 
   useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')])
-      .then(all => {
-        setState(prev => ({
-          ...prev,
-          days: all[0].data,
-          appointments: all[1].data,
-          interviewers: all[2].data,
-        }));
-      });
+    Promise.all([axios.get('/api/days'), axios.get('/api/appointments'), axios.get('/api/interviewers')]).then(all => {
+      setState(prev => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+    });
   }, []);
 
   const setDay = day => setState({ ...state, day });
-  const currentDay = state.days.filter(x => state.day === x.name)[0];
+  const currentDay = state.days.filter(day => state.day === day.name)[0];
+
+  const updateSpots = (state, appointments) => {
+    let days = state.days;
+    let currentAppointments = 0;
+    const currentDay = state.days.find(day => day.name === state.day);
+
+    currentDay.appointments.map(appointment => {
+      if (appointments[appointment].interview !== null) {
+        currentAppointments += 1;
+      }
+    });
+    currentDay.spots = 5 - currentAppointments;
+
+    return days;
+  };
+  //This function is working but I don't see how days is being altered, or why I can't just immediately return line 27 without running the function.
+
+  /*  Find the current day by filtering state.days - DONE
+For each appointment in currentDay
+Push the appointment from APPOINTMENTS into an array
+filter the array to show interviews that are not null
+Turn that to a number
+Have the current day's spots equal 5 minus the number in the line above */
 
   const bookInterview = (id, interview) => {
     const appointment = {
@@ -39,8 +58,10 @@ export default function useApplicationData() {
 
     return axios
       .put(`/api/appointments/${id}`, { interview })
-      .then(() => setState({ ...state, appointments }))
-      .then((state.days[currentDay.id - 1].spots -= 1))
+      .then(() => {
+        const days = updateSpots(state, appointments);
+        setState({ ...state, appointments, days });
+      })
       .catch(err => err);
   };
 
@@ -55,8 +76,10 @@ export default function useApplicationData() {
     };
     return axios
       .delete(`/api/appointments/${id}`)
-      .then(() => setState({ ...state, appointments }))
-      .then((state.days[currentDay.id - 1].spots += 1))
+      .then(() => {
+        const days = updateSpots(state, appointments);
+        setState({ ...state, appointments, days });
+      })
       .catch(err => err);
   };
 
